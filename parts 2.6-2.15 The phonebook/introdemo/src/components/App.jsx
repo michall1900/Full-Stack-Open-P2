393a90f6086e300a8a7dbc49500f55d7ba202d46
Michal Bucks <michall1900@gmail.com>
2025-01-29 16:45:06 +0200

    Finish with part 2.6.

--- parts 2.6-2.15 The phonebook/introdemo/src/components/App.jsx
+++ parts 2.6-2.15 The phonebook/introdemo/src/components/App.jsx
@@ -1,24 +1,36 @@
 import { useState } from 'react'
+import shortid from 'shortid'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
-  ]) 
+  const [persons, setPersons] = useState([{ name: 'Arto Hellas', id: shortid.generate()}]) 
   const [newName, setNewName] = useState('')
 
+
+  const addPerson = (event) => {
+    event.preventDefault();
+
+    setPersons(persons.concat({name: newName, id: shortid.generate()}));
+    setNewName('');
+  }
+
+  const changeName = (event) => {
+    setNewName(event.target.value);
+  }
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={addPerson}>
         <div>
-          name: <input />
+          name: <input value={newName} onChange={changeName}/>
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      <ul> 
+        {persons.map(({name, id}) => <li key={id}>{name}</li> )}
+      </ul>
     </div>
   )
 }
