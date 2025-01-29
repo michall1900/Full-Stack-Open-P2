42fccc30a480fbe04ec61d9e28345b3fddf6263d
Michal Bucks <michall1900@gmail.com>
2025-01-29 12:58:07 +0200

    Complete part 2.1

---
+++ parts 2.1- 2.5 Course information/introdemo/src/components/Course.jsx
@@ -0,0 +1,23 @@
+import Header from './Header';
+import Content from './Content';
+import Total from './Total';
+
+const Course =({course})=>{
+
+    return (
+        <div>
+          <Header course={course.name} />
+          <Content parts={course.parts} />
+          <Total
+            total={
+              course.parts[0].exercises +
+              course.parts[1].exercises +
+              course.parts[2].exercises
+            }
+          />
+        </div>
+      )
+}
+
+
+export default Course;
